58039fd534384ac8c1c8e98863558c64a06a6f09
Tharamani <tharamani15@gmail.com>
2022-12-28 23:11:45 +0800

    Modified code to evaluate expression for special forms(quote, define)

--- lisp.js
+++ lisp.js
@@ -53,17 +53,25 @@ const globalEnv = {
   sqrt: (...args) => Math.sqrt(args[0])
 }
 
+const stringParser = (input) => {
+  console.log('stringParser', input)
+  const matched = input.match(/".*"/)
+  console.log('stringParser matched', matched)
+  if (matched) return matched[0].trim()
+  return null
+}
+
 // number parser
 const numberParser = input => {
   console.log('numberParser', input)
   const output = input.match(/^[+-]?((([1-9])(\d*))|(0))(\.\d+)?/)
-  console.log('numberParser', output)
   if (output) return [Number(output[0]), input.slice(output[0].length).trim()]
   return null
 }
 
-const builtInFunctionParser = (input) => {
-  console.log('builtInFunctionParser', input, globalEnv[input])
+// function parser
+const functionParser = (input) => {
+  console.log('functionParser', input, globalEnv[input])
   if (globalEnv[input] === undefined) return null
   return globalEnv[input]
 }
@@ -82,37 +90,174 @@ const symbolParser = (input) => {
     input = input.slice(1)
   }
   console.log('symbolParser lFunction', [lFunction, input.slice(1).trim()])
-  if (globalEnv[lFunction] !== undefined) return [lFunction, input.slice(1).trim()]
-  return null
+  // if (globalEnv[lFunction] !== undefined)
+  return [lFunction, input.slice(1).trim()]
+  // return null
+}
+
+const expressionParser = (input) => {
+  console.log('expressionParser', input)
+  let expressionStr = input.slice(1) // input = (...) ) // expression = ...) )
+  let count = 1
+  while (count !== 0) {
+    if (expressionStr[0] === '(') count++
+    if (expressionStr[0] === ')') count--
+    expressionStr = expressionStr.slice(1)
+  }
+  console.log('str', expressionStr.trim())
+  console.log(input.slice(0, input.length - expressionStr.length).trim(), expressionStr.trim())
+  return [input.slice(0, input.length - expressionStr.length).trim(), expressionStr.trim()]
+}
+
+// Quote eval
+// (quote exp)
+// (quote (+ 1 2))
+// null
+// [value, remaining string]
+const quoteEval = (input) => {
+  if (!input.startsWith('(')) return null
+  input = input.slice(1).trim() // slice '('
+
+  console.log('quoteEval', input)
+
+  if (!input.startsWith('quote')) return null
+  input = input.slice('quote'.length).trim() // slice 'quote'
+
+  const parsed = expressionParser(input)
+  if (parsed === null) return null
+  const value = parsed[0]
+  input = parsed[1]
+
+  if (input !== ')') return null
+  return [value, input.slice(input.length)]
+}
+
+// Define eval
+// syntax: (define symbol exp)
+// null
+// [value, remaining string]
+const defineEval = (input, env) => {
+  if (!input.startsWith('(define')) return null
+
+  console.log('defineEval', input)
+  input = input.slice('(define'.length) // slice '(define'
+
+  let parsed = symbolParser(input)
+  console.log('defineEval', parsed)
+  if (parsed === null) return null
+
+  const variable = parsed[0] // variable
+  input = parsed[1]
+  console.log('defineEval variable', variable)
+
+  parsed = expressionEval(input)
+  console.log('defineEval parsed', parsed)
+  if (parsed === null) return null
+
+  const expression = parsed[0] // value/expression
+  input = parsed[1]
+  console.log('defineEval expression', expression)
+
+  console.log('defineEval input', input)
+  if (input[0] !== ')') return null
+
+  env[variable] = expression// expressionEval(expression, env)
+
+  console.log(`${variable} defined`)
+
+  return [null, input.slice(1)]
+}
+
+// if Eval
+// (if testExpr conseqExpr altExpr)
+// testExpr = exprParser(input)
+// conseqExpr = exprParser(input)
+// altExpr = exprParser(input)
+// if (evalExpr(testExpr)) return evalExpr(conseqExpr)
+// return evalExpr(altExpr)
+// (if (> 3 4) 3 2)
+// [3, remainingstring]
+// [2, remainingstring]
+const ifEval = (input, env) => {
+  console.log('ifEval input', input)
+  if (!input.startsWith('(')) return null
+
+  input = input.slice(1) // slice '('
+
+  let parsed = expressionEval(input)
+  console.log('ifEval parsed', parsed)
+
+  const ifSF = parsed[0]
+  console.log('ifEval ifSF', ifSF)
+  if (ifSF !== 'if') return null
+
+  input = parsed[1]
+  console.log('ifEval input', input)
+
+  parsed = expressionEval(input)
+  console.log('ifEval parsed', parsed)
+
+  const test = parsed[0]
+  console.log('ifEval test', test)
+
+  input = parsed[1]
+  console.log('ifEval input', input)
+
+  parsed = expressionEval(input)
+  console.log('ifEval parsed', parsed)
+
+  const consequent = parsed[0]
+  console.log('ifEval consequent', consequent)
+
+  input = parsed[1]
+  console.log('ifEval input', input)
+
+  parsed = expressionEval(input)
+  console.log('ifEval parsed', parsed)
+
+  const alternate = parsed[0]
+  console.log('ifEval alternate', alternate)
+
+  input = parsed[1]
+  console.log('ifEval input', input)
+
+  if (input[0] !== ')') return null
+  if (test) return consequent
+  return alternate
 }
 
 let count = 0
 // compound eval
-const compoundEval = (input) => {
+const functionEval = (input) => {
   if (!input.startsWith('(')) return null
   count++
 
-  console.log('compoundEval', input)
+  console.log('functionEval', input)
   input = input.slice(1) // slice '('
 
   const parsed = expressionEval(input)
   if (parsed === null) return null
 
   const lFunction = parsed[0] // lFunction
+  console.log('functionEval parsed', parsed, lFunction, parsed[1])
   if (globalEnv[lFunction] === undefined) return null
-
   input = parsed[1] // args in string
   const output = []
-  console.log('compoundEval parsed', parsed, lFunction, parsed[1])
+
+  // if (specialForms.includes(lFunction)) return formParser(lFunction, input, globalEnv)
 
   while (input[0] !== ')' && input[0]) {
     // if (expression) {
     const args = expressionEval(input)
     // console.log('compoundEval args, args[0], args[1]', args, args[0], args[1])
-    if (args === null || globalEnv[args[0]]) return null
-    output.push(args[0])
+    console.log('functionEval args, args[0], args[1]', args, args[0], args[1], globalEnv[args[0]])
+    if (args === null) return null
+    if (args[0] === 'pi') output.push(globalEnv[args[0]])
+    else {
+      output.push(args[0])
+    }
     input = args[1]
-    console.log('compoundEval input', output, input)
+    console.log('functionEval input', output, input)
     // }
   }
   if (input[0] === ')') {
@@ -126,20 +271,51 @@ const compoundEval = (input) => {
   return null
 }
 
+// null, [value, remainingstring]
 const expressionEval = (input) => {
   console.log('expressionEval', input)
   input = input.trim()
   console.log('expressionEval trim', input)
 
-  if (builtInFunctionParser(input) === false) return false
-  return numberParser(input) || builtInFunctionParser(input) || symbolParser(input) || compoundEval(input)
+  // if (input[0] === '\'') input = input.slice(1)
+  if (input[0] === '\'') return input.slice(1)
+  if (functionParser(input) === false) return false
+  console.log('expressionEval ', input)
+  // return null, [value, remaining string]
+  return numberParser(input) || functionParser(input) || symbolParser(input) || functionEval(input) || defineEval(input, globalEnv) || quoteEval(input) || ifEval(input) // || stringParser(input)
 }
 
+// console.log(expressionEval('(+ 2 3 (if (> 3 2) 3 2) (+ 2))'))
+// console.log(expressionEval('(if (= 12 12) (+ 78 2) 9)'))
+// console.log(expressionEval('(if #f 1 0)'))
+// console.log(expressionEval('(if #t "abc" 1)'))
+// console.log(expressionEval('(if (> 30 45) (+ 1 1) "failedOutput" )'))
+// console.log(expressionEval('(if (< 3 2) 3 2)'))
+// console.log(expressionEval('\'(+ 1 2)'))
+// console.log(expressionEval('\'abc'))
+// console.log(expressionEval('\'#(a b c)'))
+// console.log(expressionEval('\'abc'))
+// console.log(expressionEval('\'\'a'))
+console.log(expressionEval('(quote (+ 1 2))'))
+// console.log(expressionEval('(quote a)'))
+// console.log(expressionEval('(quote #(a b c))'))
+// console.log(expressionEval('(quote (+ 1 2))'))
+// console.log(expressionEval('(* pi 2)'))
+// console.log(expressionEval('(define circle-area (* pi 2))'))
+// console.log('lisp>>>>>', expressionEval('circle-area'))
+// console.log(expressionEval('(+ 2 (define x (+ 2 10(+ 2 2))))'))
+// console.log(expressionEval('(define x (+ 2 10(* pi 2)))'))
+// console.log(expressionEval('(define x (+ 2 10))'))
+// console.log(expressionEval('(define x (+ 2 10(+ 2 2)))'))
+// console.log('lisp>>>>>', expressionEval('(define x 10)'))
+// console.log('lisp>>>>>', expressionEval('x'))
+// console.log(expressionEval('(definex 10)'))
 // console.log(expressionEval('#t'))
 // console.log(expressionEval('#f'))
 // console.log(expressionEval('pi'))
 // console.log(expressionEval('+'))
 // console.log(expressionEval('-'))
+// console.log(expressionEval('<='))
 // console.log(expressionEval('(sqrt 4)'))
 // console.log(expressionEval('2'))
 // console.log(expressionEval('22'))
